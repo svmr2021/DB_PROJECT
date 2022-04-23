4a6c3245fb5d7c835315d3793106c837a3306a26
svmr <saidamir2020t@gmail.com>
2022-04-23 14:24:40 +0500

    models done

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+@admin.register(Client)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company_name')
+
+
+@admin.register(Employee)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('user', 'identifier')
+
+
+@admin.register(Location)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('country', 'latitude', 'longitude', 'region')
+
+
+@admin.register(Air)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'humidity', 'temperature', 'pressure')
+
+
+@admin.register(Water)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'temperature', 'radioactivity')
+
+
+@admin.register(Soil)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'moisture', 'salinity', 'erosion')
+
+
+@admin.register(Noise)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'level')
+
+
+@admin.register(Plant)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'type', 'amount')
+
+
+@admin.register(Animal)
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('location', 'type', 'amount')
--- core/models.py
+++ core/models.py
@@ -31,10 +31,16 @@ class Client(Base):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'{self.company_name}'
+
 
 class Employee(Base):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     identifier = models.PositiveIntegerField(unique=True)
+
+    def __str__(self):
+        return f'{self.identifier}'
 
 
 class Location(Base):
@@ -43,16 +49,32 @@ class Location(Base):
     longitude = models.CharField(max_length=255)
     region = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'{self.country}-{self.region}'
+
+    class Meta:
+        ordering = ['-created_at',]
+
 
 class Air(ElementBase):
     humidity = models.FloatField(default=0)
     temperature = models.FloatField(default=0)
     pressure = models.FloatField(default=0)
 
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Air data'
+        verbose_name_plural = 'Air info'
+
 
 class Water(ElementBase):
     temperature = models.FloatField(default=0)
     radioactivity = models.FloatField(default=0)
+
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Water data'
+        verbose_name_plural = 'Water info'
 
 
 class Soil(ElementBase):
@@ -60,17 +82,37 @@ class Soil(ElementBase):
     salinity = models.FloatField(default=0)
     erosion = models.FloatField(default=0)
 
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Soil data'
+        verbose_name_plural = 'Soil info'
+
 
 class Noise(ElementBase):
     level = models.FloatField(default=0)
+
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Noise data'
+        verbose_name_plural = 'Noise info'
 
 
 class Plant(ElementBase):
     type = models.CharField(max_length=255)
     amount = models.IntegerField(default=0)
 
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Plant data'
+        verbose_name_plural = 'Plant info'
+
 
 class Animal(ElementBase):
     type = models.CharField(max_length=255)
     amount = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['-created_at', ]
+        verbose_name = 'Animal data'
+        verbose_name_plural = 'Animal info'
 
