7994a3feae86337584703a90e045bf1e44dae969
svmr <saidamir2020t@gmail.com>
2022-04-25 03:52:06 +0500

    minor fix

--- db_project/urls.py
+++ db_project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import core.urls
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
@@ -20,6 +21,7 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
+    path('', LoginView.as_view(template_name='login.html')),
     path('admin/', admin.site.urls),
     path('core/', include('core.urls')),
 ]
